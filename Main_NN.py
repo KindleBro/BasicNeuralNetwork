8e9632475f389dc265c992c58211b4e4acaee023
KindleBro <65762515+KindleBro@users.noreply.github.com>
2020-05-22 23:30:46 +1200

    Update Main_NN.py
    Moved epoch to training function instead of class variable.

--- Main_NN.py
+++ Main_NN.py
@@ -27,11 +27,11 @@ def generate_nn_pair() -> tuple:
 
 
 # setup
-iterations = 20400
-epochs = 100
-batches_per_epoch = 400
-learning_rate = 1
-test_size = 10
+iterations = 200
+epochs = 3
+batches_per_epoch = 4
+learning_rate = 3
+test_size = 5
 
 # Generating inputs and outputs
 inputs = np.zeros((1, iterations))
@@ -51,16 +51,13 @@ for i in range(test_size):
 
 # main
 if __name__ == "__main__":
-	# TODO: Optimize input and output neural network architecture down to a tuple (list?).
-	nn_architecture = [
-		{"input_dim": 1, "output_dim": 2, "activation": "sigmoid"},
-		{"input_dim": 2, "output_dim": 1, "activation": "sigmoid"},
-	]
+	nn_architecture = [{"input_dim": 1, "output_dim": 2, "activation": "sigmoid"},
+					   {"input_dim": 2, "output_dim": 1, "activation": "sigmoid"}]
 	
-	nn = NeuralNetwork("Test_Network", should_debug_log=True)
-	nn.new(nn_architecture, learning_rate=learning_rate, epochs=epochs)
-	parameter_values, cost_history, accuracy_history = nn.train(inputs, outputs, batch_size=batches_per_epoch)
-	
+	nn = NeuralNetwork("Test_Network", should_debug_log=False)
+	nn.new(nn_architecture, learning_rate=learning_rate)
+	parameter_values, cost_history, accuracy_history = nn.train(inputs, outputs,
+	                                                            epochs=epochs, batch_size=batches_per_epoch)
 	for i in range(test_size):
 		prediction, _ = nn.full_forward_propagation(test_in[0][i])
 		print("I: {:.2f}, ({} vs {:.2f})".format(test_in[0][i], test_out[0][i], prediction[0][0]))
